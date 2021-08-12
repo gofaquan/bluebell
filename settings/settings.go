058c1c8e3883927d57e8c5833e2b73c68ea40a00
kashimashino <2660796265@qq.com>
2021-08-12 20:29:47 +0800

    add config.yaml into conf dir ,change code to adapt

--- settings/settings.go
+++ settings/settings.go
@@ -48,10 +48,11 @@ type RedisConfig struct {
 
 func Init() (err error) {
 
-	viper.SetConfigFile("config.yaml")
+	viper.SetConfigFile("./conf/config.yaml")
 	//viper.SetConfigName("config") // 指定配置文件名称（不需要带后缀）
 	//viper.SetConfigType("yaml")   // 指定配置文件类型(专用于从远程获取配置信息时指定配置文件类型的)
-	viper.AddConfigPath(".")   // 指定查找配置文件的路径（这里使用相对路径）
+	viper.AddConfigPath("./conf") // 指定查找配置文件的路径（这里使用相对路径）
+
 	err = viper.ReadInConfig() // 读取配置信息
 	if err != nil {
 		// 读取配置信息失败
