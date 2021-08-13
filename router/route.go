c6c7e476934e98468c13f9050e668da5102bccff
kashimashino <2660796265@qq.com>
2021-08-13 15:44:46 +0800

    get all community sorts ok!

--- router/route.go
+++ router/route.go
@@ -28,6 +28,24 @@ func SetupRouter(mode string) *gin.Engine {
 		})
 	})
 
+	v1 := r.Group("/api/v1")
+	// 根据时间或分数获取帖子列表
+	//v1.GET("/posts2", controller.GetPostListHandler2)
+	//v1.GET("/posts", controller.GetPostListHandler)
+	v1.GET("/community", controller.CommunityHandler)
+	//v1.GET("/community/:id", controller.CommunityDetailHandler)
+	//v1.GET("/post/:id", controller.GetPostDetailHandler)
+	//
+	//v1.Use(middlewares.JWTAuthMiddleware()) // 应用JWT认证中间件
+	//
+	//{
+	//	v1.POST("/post", controller.CreatePostHandler)
+	//
+	//	// 投票
+	//	v1.POST("/vote", controller.PostVoteController)
+	//}
+	//
+
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "404",
